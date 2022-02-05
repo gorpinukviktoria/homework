b3e320849ad0e2d9822956cae0522e35573f2a63
gorpinukviktoria <95917993+gorpinukviktoria@users.noreply.github.com>
2022-02-06 00:31:21 +0200

    Add files via upload

---
+++ diplom/script/script.js
@@ -0,0 +1,33 @@
+$('.about-items > div').on('click', function(){
+    $(this).children('.about-icon').toggleClass('active');
+    $(this).parent('.about-items').toggleClass('toggleBg');
+    $(this).toggleClass('toggleColor');
+    $(this).next('p').slideToggle(300).toggleClass('toggleColor');
+})
+
+$('.features-items > div').on ('click', function(){
+    $(this).toggleClass('toggleColor-ic');
+    $(this).parent('.features-items').toggleClass('toggleBack');
+    $(this).next('p').slideToggle(300)
+})
+
+$('.container-up').slick({ 
+    dots: true,
+})
+
+$(window).scroll(function(){
+    if($(this).scrollTop() >500){
+        $('#up').fadeIn();
+    }else{
+        $('#up').fadeOut();
+    }
+})
+
+$('#up').on('click', function(){
+    $('html, body').animate({scrollTop: 0}, 1000)
+})
+
+$('#home').on('click', function(e){
+    e.preventDefault()
+    $('html, body').animate({scrollTop: $('#about').offset().top}, 500)
+})
